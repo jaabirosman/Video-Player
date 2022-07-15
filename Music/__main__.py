ab6f5be85b2f9b2cebd9c0bbc9ff471be37b8426
ribfirst <108966423+ribfirst@users.noreply.github.com>
2022-07-15 06:46:39 +0300

    Update __main__.py

--- Music/__main__.py
+++ Music/__main__.py
@@ -46,9 +46,9 @@ async def load_start():
             await remove_active_chat(served_chat)
         except Exception:
             print("Error came while clearing db")
-    await app.send_message(LOG_GROUP_ID, "Osmani Bot Started")
-    await client.send_message(LOG_GROUP_ID, "Osmani Assistant Started")
-    await client.join_chat("Superior_Bots")
+    await app.send_message(LOG_GROUP_ID, "-1001150107625")
+    await client.send_message(LOG_GROUP_ID, "-1001150107625")
+    await client.join_chat("osmanigroupbot")
     print("[INFO]: STARTED")
 
 
